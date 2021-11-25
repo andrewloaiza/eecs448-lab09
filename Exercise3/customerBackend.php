001707cbdb223437c733ec9e0c3df737d3189b86
Andrew Loaiza <a358l152@ku.edu>
2021-11-24 20:50:26 -0600

    finished exercise 3

--- Exercise3/customerBackend.php
+++ Exercise3/customerBackend.php
@@ -2,15 +2,103 @@
 
 error_reporting(E_ALL);
 ini_set("display_errors", 1);
-echo "<link href='styles.css' rel='stylesheet' type='text/css'/>";
+echo "<link href='style.css' rel='stylesheet' type='text/css'/>";
 
 
+// set default time zone
+date_default_timezone_set('America/Chicago');
 
 
-echo "<title>Invoice</title>";
+// get shipping option information
+$shipPrice = (int)$_POST["shipping"];
+$shipDays  = 0;
 
-echo "<h1>Welcome!</h1>";
+if ($shipPrice == 0)      $shipDays = 7;
+else if ($shipPrice == 5) $shipDays = 3;
+else                      $shipDays = 1;
 
-echo "<p>Password: " . $_POST["password"] . "</p>";
+
+// item quantity, price, and total
+$chNum = (int)$_POST["chance_cap"];
+$kuNum = (int)$_POST["ku_cap"];
+$kcNum = (int)$_POST["kc_cap"];
+
+$chPrice = 10;
+$kuPrice = 15;
+$kcPrice = 20;
+
+$chTotal = $chNum * $chPrice;
+$kuTotal = $kuNum * $kuPrice;
+$kcTotal = $kcNum * $kcPrice;
+
+
+// calculate total
+$total = $shipPrice + $chTotal + $kuTotal + $kcTotal;
+
+
+// echo receipt information on page
+echo "<title>Receipt</title>";
+
+echo "<header>
+        <div class='company-name'>Andrew's Cap Store</div>
+        <div class='receipt'>Receipt</div>
+      </header>
+
+
+      <div class='body'>
+
+        <p class='address'> Eaton Hall <br> 1520 W 15th St <br> Lawrence, KS 66045 </p>
+
+        <p>
+          <b>Merchant's Note: </b><br>
+          Thanks for shopping at Andrew's Cap Store! <br>
+          Purchase details below, feel free to print page.<br>
+          You can expect your purchase to arrive in " . $shipDays . " day(s).<br>
+          Password: " . $_POST["password"] . "<br><br><br>
+
+
+        Date: " . date('m/d/Y') . " (CT)<br>
+        Receipt #: " . rand(10000, 99999) . "<br><br>
+        Bill to: 123 Example Ave, Lawrence, KS 66045<br>
+        Ship to: 123 Example Ave, Lawrence, KS 66045<br>
+
+        <table class='receiptTable'>
+          <tr>
+            <th>Qty</th>
+            <th>Description</th>
+            <th>Unit Price</th>
+            <th>Cost</th>
+          </tr>
+          <tr>
+            <td>" . $chNum . "</td>
+            <td>Chance 3 Cap</td>
+            <td>$" . $chPrice . "</td>
+            <td>$" . $chTotal . "</td>
+          </tr>
+          <tr>
+            <td>" . $kuNum . "</td>
+            <td>University of Kansas Jayhawk Cap</td>
+            <td>$" . $kuPrice . "</td>
+            <td>$" . $kuTotal . "</td>
+          </tr>
+          <tr>
+            <td>" . $kcNum . "</td>
+            <td>Kansas City Royals Cap</td>
+            <td>$" . $kcPrice . "</td>
+            <td>$" . $kcTotal . "</td>
+          </tr>
+          <tr>
+            <td colspan='2'></td>
+            <td>" . $shipDays . "-Day Shipping</td>
+            <td>$" . $shipPrice . "</td>
+          </tr>
+          <tr>
+            <td colspan='2'></td>
+            <td><b>TOTAL</b></td>
+            <td>$" . $total . "</td>
+          </tr>
+        </table>
+
+      </div>";
 
 ?>
--- Exercise3/formChecker.js
+++ Exercise3/formChecker.js
@@ -1,9 +1,32 @@
-// source:  https://www.simplilearn.com/tutorials/javascript-tutorial/email-validation-in-javascript
+// source:  https://stackoverflow.com/questions/46155/whats-the-best-way-to-validate-an-email-address-in-javascript?rq=1
 // param:   string containing email address
 // return:  Boolean whether email is in valid format
 function validEmail(email) {
-    const re = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
-    return re.test(String(email).toLowerCase());
+  const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return (re.test(String(email).toLowerCase()));
+}
+
+
+// resets all entries in form
+function reset()
+{
+  document.querySelector("form").reset();
+}
+
+
+// check that at least one cap was chosen
+function validItemNum()
+{
+  let numChance = document.getElementById("chance_cap").value;
+  let numKU     = document.getElementById("ku_cap").value;
+  let numKC     = document.getElementById("kc_cap").value;
+
+  if (numChance <= 0 && numKU <= 0 && numKC <= 0)
+  {
+    return false;
+  }
+
+  return true;
 }
 
 
@@ -12,10 +35,25 @@ function checkForm()
 {
   let email = document.getElementById("email").value;
 
+  // check if email is valid
   if (!validEmail(email))
   {
     event.preventDefault();
-    alert("Invalid email address!");
+    alert("ERROR: Invalid email address");
+    document.querySelector(".email-error").style.display = "block";
+    return;
+  }
+
+  else
+  {
+    document.querySelector(".email-error").style.display = "none";
+  }
+
+  // check that at least one item is picked
+  if (!validItemNum())
+  {
+    event.preventDefault();
+    alert("ERROR: You did not choose any caps!");
     return;
   }
 
